5d381d171855ff12d997efe87746d172d5c9c0bc
Diamond Rivero <diamant3@proton.me>
2024-12-18 15:04:50 +0800

    use `-fno-enforce-eh-specs`

--- scripts/005-gcc-stage2.sh
+++ scripts/005-gcc-stage2.sh
@@ -70,7 +70,7 @@ cd build-$TARGET-stage2
   --enable-threads=posix \
   --disable-tls \
   --disable-nls \
-  --enable-cxx-flags="-fnothrow-opt" \
+  --enable-cxx-flags="-fno-enforce-eh-specs" \
   $TARG_XTRA_OPTS
 
 ## Compile and install.
